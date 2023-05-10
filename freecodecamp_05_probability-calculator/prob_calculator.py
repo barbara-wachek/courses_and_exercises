5439e1a68d98c2cd8cff0dfd90e2f4e82d83526d
barbara-wachek <barbara-wachek@gmail.com>
2023-05-10 20:51:10 +0200

    2023-05-10

--- freecodecamp_05_probability-calculator/prob_calculator.py
+++ freecodecamp_05_probability-calculator/prob_calculator.py
@@ -26,57 +26,65 @@ class Hat:
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    hat = copy.copy(hat) #w razie problemów spróbować z deepcopy
+    hat = copy.copy(hat) 
     list_of_balls = []
     
+    hat = Hat(yellow=5,red=1,green=3,blue=9,test=1)
+    #experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
+    num_balls_drawn=20
+    num_experiments=100
+    expected_balls={"yellow":2,"blue":3,"test":1}
     
-    # num_balls_drawn=5
-    # num_experiments=2000
-    # hat = Hat(black=6, red=4, green=3)
-    # expected_balls={"red":2,"green":1}
-    
-
     counter = 0
-    control_number = 0   
+    
     
     for x in range(num_experiments):
         
-        test = random.choices(hat.contents, k=num_balls_drawn)
-        test_dictionary = {}
+        hat_contents = copy.copy(hat.contents) 
+        test_list = []
         
-        for element in test:
+        for x in range(num_balls_drawn):
+            if hat_contents != []:
+                chosen_element = random.choice(hat_contents)
+                hat_contents.remove(chosen_element)
+                test_list.append(chosen_element)
+            else:
+                pass
+            
+        #test = random.choices(hat.contents, k=num_balls_drawn)
+        
+        test_dictionary = {}
+        #control_number = 0  
+         
+        
+        for element in test_list:
             if element not in test_dictionary.keys(): 
                 test_dictionary[element] = 1
             else:
                 test_dictionary[element] += 1
         
-        
-        for key, value in expected_balls.items():
-            for k, v in test_dictionary.items():
-                if key == k and value == v: 
-                    control_number += 1
+   
+        # for key, value in expected_balls.items():
+        #     for k, v in test_dictionary.items():
+        #         if key == k and value == v: 
+        #             control_number += 1
                     
-        if control_number == len(expected_balls.keys()):   
-            counter += 1 
+        # if control_number == len(expected_balls.keys()):   
+        #     counter += 1 
                 
         
-        # if expected_balls in test_dictionary:
-        #     counter = counter + 1
-    
+        #to wyżej ejst źle 
+        
+        
+        
     probability = counter/num_experiments       
             
-        # for element in list_of_balls: 
-            
-        #     #wyjmij 5 random balls ze zbioru
-        #     #sprawdz czy wyjete kule sa expected_balls
-        #         #Jesli tak - counter + 1
-        #     list_of_balls.append(random.randrange(" ".join(hat.contents)))
-
     return probability
 
 
 
-
+hat = Hat(yellow=5,red=1,green=3,blue=9,test=1)
+experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
 
 
 #%% main
@@ -118,6 +126,39 @@ probability = prob_calculator.experiment(
     num_balls_drawn=4,
     num_experiments=3000)
 print("Probability:", probability)
+
+
+
+
+
+
+
+
+hat = Hat(red=5,blue=2)
+actual = hat.draw(2)
+expected = ['blue', 'red']
+        self.assertEqual(actual, expected, 'Expected hat draw to return two random items from hat contents.')
+        actual = len(hat.contents)
+        expected = 5
+        self.assertEqual(actual, expected, 'Expected hat draw to reduce number of items in contents.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
